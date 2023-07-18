a4d489bd143585b5c5be6a0b5b28e85195f7543c
Paulo Abreu <paulo.santana@luby.software>
2023-07-18 18:09:02 -0300

    test: ensure thats possible create new customer on success

---
+++ src/core/application/usecases/__tests__/register-customer.usecase.spec.ts
@@ -0,0 +1,40 @@
+import { CustomerRepository } from '@domain/repositories/customer-repository.interface';
+import { RegisterCustomerUseCase } from '../register-customer.usecase';
+import { InMemoryCustomerRepository } from './repositories/in-memory-customer-repository';
+import { InMemoryHasherGateway } from './repositories/in-memory-hasher-gateway';
+
+describe('Register Customer UseCase', () => {
+  let usecase: RegisterCustomerUseCase;
+  let customerRepository: CustomerRepository;
+
+  const payload = {
+    name: 'John Doe',
+    email: 'johndoe@email.com',
+    plainTextPassword: '12345678',
+  };
+
+  beforeEach(() => {
+    const hasher = new InMemoryHasherGateway();
+
+    customerRepository = new InMemoryCustomerRepository();
+    usecase = new RegisterCustomerUseCase(customerRepository, hasher);
+  });
+
+  it('should create a new customer on success', async () => {
+    await usecase.execute(payload);
+
+    const customer = customerRepository['customers'][0];
+
+    expect(customer).toBeDefined();
+    expect(customer).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name: payload.name,
+        email: payload.email,
+        password: expect.any(String),
+        createdAt: expect.any(Number),
+        updatedAt: expect.any(Number),
+      }),
+    );
+  });
+});
