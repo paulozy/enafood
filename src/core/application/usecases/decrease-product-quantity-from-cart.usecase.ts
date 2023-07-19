e213c719628a29fb78218aa49b62e3cc558e240c
Paulo Abreu <paulo.santana@luby.software>
2023-07-19 17:44:05 -0300

    feat: create decrease product quantity from cart usecase

---
+++ src/core/application/usecases/decrease-product-quantity-from-cart.usecase.ts
@@ -0,0 +1,30 @@
+import { Cart } from '@domain/entities/cart.entity';
+import { CustomerRepository } from '@domain/repositories/customer-repository.interface';
+import { CustomerNotFoundError } from './@errors/customer-not-found-error';
+
+type DecreaseProductQuantityFromCartInput = {
+  customerId: string;
+  productId: string;
+};
+
+export class DecreaseProductQuantityFromCartUseCase {
+  constructor(private readonly customerRepository: CustomerRepository) {}
+
+  async execute(data: DecreaseProductQuantityFromCartInput): Promise<Cart> {
+    const { customerId, productId } = data;
+
+    const customer = await this.customerRepository.findById(customerId);
+
+    if (!customer) {
+      throw new CustomerNotFoundError(customerId);
+    }
+
+    const cart = customer.cart;
+
+    cart.decreaseProductQuantity(productId);
+
+    await this.customerRepository.save(customer);
+
+    return cart;
+  }
+}
