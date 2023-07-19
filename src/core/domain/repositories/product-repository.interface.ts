aee2f1362ba2b147bc647e5af8a8efc1edc9a0ef
Paulo Abreu <paulo.santana@luby.software>
2023-07-19 14:42:42 -0300

    feat: create product repository interface

--- src/core/domain/repositories/product-repository.interface.ts
+++ src/core/domain/repositories/product-repository.interface.ts
@@ -2,7 +2,7 @@ import { Product } from '@domain/entities/product.entity';
 
 export abstract class ProductRepository {
   abstract exists(id: string): Promise<boolean>;
-  abstract save(id: string, name: string, price: number): Promise<void>;
+  abstract save(product: Product): Promise<void>;
   abstract list(): Promise<Product[]>;
   abstract findById(id: string): Promise<Product | undefined>;
   abstract findByName(name: string): Promise<Product | undefined>;
