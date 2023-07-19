76fdaa0dd168afd6385bef6f7a46e5aa1740b7c3
Paulo Abreu <paulo.santana@luby.software>
2023-07-19 14:38:43 -0300

    refactor: improve order entity properties

--- src/core/domain/entities/order.entity.ts
+++ src/core/domain/entities/order.entity.ts
@@ -1,39 +1,44 @@
 import { BaseEntity } from '@shared/domain/entities/base.entity';
-import { Product } from './product.entity';
+
+type OrderProduct = {
+  productId: string;
+  quantity: number;
+};
 
 type OrderProps = {
   id?: string;
-  products: Product[];
+  customerId: string;
+  products: OrderProduct[];
+  total: number;
   createdAt?: number;
   updatedAt?: number;
 };
 
 export class Order extends BaseEntity {
-  private _products: Product[];
+  private _products: OrderProduct[];
+  private _total: number;
 
-  private constructor({ id, products, createdAt, updatedAt }: OrderProps) {
+  private constructor({
+    id,
+    products,
+    total,
+    createdAt,
+    updatedAt,
+  }: OrderProps) {
     super({ id, createdAt, updatedAt });
-
     this._products = products;
+    this._total = total;
   }
 
   static create(props: OrderProps): Order {
     return new Order(props);
   }
 
-  static calculateTotal(products: Product[]): number {
-    return products.reduce((acc, curr) => acc + curr.price, 0);
-  }
-
-  get total(): number {
-    return this._products.reduce((acc, curr) => acc + curr.price, 0);
-  }
-
-  get products(): Product[] {
+  get products(): OrderProduct[] {
     return this._products;
   }
 
-  addProduct(product: Product): void {
-    this._products.push(product);
+  get total(): number {
+    return this._total;
   }
 }
