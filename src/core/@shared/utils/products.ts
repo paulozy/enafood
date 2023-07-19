8d0db050e3a2bc5f91ebd1755d6db3bdea2f535c
Paulo Abreu <paulo.santana@luby.software>
2023-07-19 14:56:20 -0300

    feat: create fake products

---
+++ src/core/@shared/utils/products.ts
@@ -0,0 +1,82 @@
+export const products = [
+  {
+    id: '71b5f21f-0c4a-4b3f-9cb6-50e51fcd44ee',
+    name: 'Product A',
+    quantity: 5,
+    price: 10.99,
+    createdAt: 1626740400000, // Unix timestamp for July 20, 2021
+    updatedAt: 1626740400000, // Unix timestamp for July 20, 2021
+  },
+  {
+    id: '5a5a61d4-bf8d-4b37-90fe-5c83e987d3e4',
+    name: 'Product B',
+    quantity: 10,
+    price: 19.99,
+    createdAt: 1626571200000, // Unix timestamp for July 18, 2021
+    updatedAt: 1626830400000, // Unix timestamp for July 21, 2021
+  },
+  {
+    id: '46ff5e13-9b76-4d17-b736-3b7edc2a7a9d',
+    name: 'Product C',
+    quantity: 2,
+    price: 8.5,
+    createdAt: 1626740400000, // Unix timestamp for July 20, 2021
+    updatedAt: 1626740400000, // Unix timestamp for July 20, 2021
+  },
+  {
+    id: '4f78f1af-58a9-4f2c-a5b6-61e2e9bb91e8',
+    name: 'Product D',
+    quantity: 7,
+    price: 15.75,
+    createdAt: 1626481200000, // Unix timestamp for July 17, 2021
+    updatedAt: 1626481200000, // Unix timestamp for July 17, 2021
+  },
+  {
+    id: '29cf0f51-39c2-421e-991c-2e1d2c10a5ed',
+    name: 'Product E',
+    quantity: 3,
+    price: 12.49,
+    createdAt: 1626654000000, // Unix timestamp for July 19, 2021
+    updatedAt: 1626654000000, // Unix timestamp for July 19, 2021
+  },
+  {
+    id: '93801f15-34f3-43df-a6b0-7115c9bc05d9',
+    name: 'Product F',
+    quantity: 8,
+    price: 9.99,
+    createdAt: 1626740400000, // Unix timestamp for July 20, 2021
+    updatedAt: 1626740400000, // Unix timestamp for July 20, 2021
+  },
+  {
+    id: 'b8a5f2c3-d5d7-4d78-94d9-b7f7563bc1b5',
+    name: 'Product G',
+    quantity: 1,
+    price: 5.99,
+    createdAt: 1626654000000, // Unix timestamp for July 19, 2021
+    updatedAt: 1626654000000, // Unix timestamp for July 19, 2021
+  },
+  {
+    id: 'b63e8f90-b7a2-45f1-94f3-782a0fe95672',
+    name: 'Product H',
+    quantity: 4,
+    price: 11.25,
+    createdAt: 1626571200000, // Unix timestamp for July 18, 2021
+    updatedAt: 1626830400000, // Unix timestamp for July 21, 2021
+  },
+  {
+    id: 'a0cd9185-d22c-46ef-8744-b68eb2e4f3eb',
+    name: 'Product I',
+    quantity: 6,
+    price: 14.99,
+    createdAt: 1626481200000, // Unix timestamp for July 17, 2021
+    updatedAt: 1626481200000, // Unix timestamp for July 17, 2021
+  },
+  {
+    id: '33bea8cc-97f4-4955-8c48-6db26e2861f0',
+    name: 'Product J',
+    quantity: 9,
+    price: 17.5,
+    createdAt: 1626571200000, // Unix timestamp for July 18, 2021
+    updatedAt: 1626830400000, // Unix timestamp for July 21, 2021
+  },
+];
