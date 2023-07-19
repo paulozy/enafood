3d032f9ee650a74dafcf7c4e04625f12891d9f3b
Paulo Abreu <paulo.santana@luby.software>
2023-07-19 14:57:27 -0300

    test: ensure thats product quantity is decreased on add a product to cart

--- src/core/application/usecases/__tests__/add-product-to-cart.usecase.spec.ts
+++ src/core/application/usecases/__tests__/add-product-to-cart.usecase.spec.ts
@@ -46,4 +46,22 @@ describe('Create Cart UseCase', () => {
     expect(cart.products[0].id).toBe(productInfo.id);
     expect(cart.products[0].quantity).toBe(1);
   });
+
+  it('should be possible add a product to cart and decrease product quantity', async () => {
+    const cart = await usecase.execute({
+      customerId: customer.id,
+      item: {
+        id: productInfo.id,
+        quantity: 1,
+      },
+    });
+
+    const product = await productRepository.findById(productInfo.id);
+
+    expect(cart).toBeDefined();
+    expect(cart.products.length).toBe(1);
+    expect(cart.products[0].id).toBe(productInfo.id);
+    expect(cart.products[0].quantity).toBe(1);
+    expect(product.quantity).toBe(4);
+  });
 });
