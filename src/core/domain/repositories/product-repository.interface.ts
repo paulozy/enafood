e5ee14f45a3b36b67050b677ddde46a89edd4e17
Paulo Abreu <paulo.santana@luby.software>
2023-07-20 16:10:23 -0300

    feat: add input type to update stockMany on product repository

--- src/core/domain/repositories/product-repository.interface.ts
+++ src/core/domain/repositories/product-repository.interface.ts
@@ -1,8 +1,14 @@
 import { Product } from '@domain/entities/product.entity';
 
+export type UpdateStockInput = {
+  productId: string;
+  quantity: number;
+};
+
 export abstract class ProductRepository {
   abstract exists(id: string): Promise<boolean>;
   abstract save(product: Product): Promise<void>;
+  abstract updateStockMany(products: UpdateStockInput[]): Promise<void>;
   abstract list(): Promise<Product[]>;
   abstract findById(id: string): Promise<Product | undefined>;
   abstract findByName(name: string): Promise<Product | undefined>;
