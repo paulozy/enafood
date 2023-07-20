4f6e18201f2f1a7015a47cec12e0e63f9a3a8d32
Paulo Abreu <paulo.santana@luby.software>
2023-07-20 11:23:03 -0300

    test: ensure thats possible add an address to a customer

---
+++ src/core/application/usecases/__tests__/add-address-to-customer.usecase.spec.ts
@@ -0,0 +1,47 @@
+import { Customer } from '@domain/entities/customer.entity';
+import { CustomerRepository } from '@domain/repositories/customer-repository.interface';
+import { AddAddressToCustomerUseCase } from '../add-address-to-customer.usecase';
+import { makeCustomer } from './factories/customer-factory';
+import { InMemoryCustomerRepository } from './repositories/in-memory-customer-repository';
+
+describe('Add Address to Customer UseCase', () => {
+  let usecase: AddAddressToCustomerUseCase;
+  let customerRepository: CustomerRepository;
+
+  let customer: Customer;
+
+  const payload = {
+    street: 'Rua A',
+    number: '123',
+    district: 'Bairro A',
+    city: 'Cidade A',
+    state: 'Estado A',
+    country: 'País A',
+    zipCode: '12345678',
+  };
+
+  beforeEach(() => {
+    customerRepository = new InMemoryCustomerRepository();
+
+    customer = makeCustomer({});
+    customerRepository.save(customer);
+
+    usecase = new AddAddressToCustomerUseCase(customerRepository);
+  });
+
+  it('should be possible add a address to customer', async () => {
+    await usecase.execute({
+      customerId: customer.id,
+      ...payload,
+    });
+
+    expect(customer.addresses.length).toBe(1);
+    expect(customer.addresses[0].street).toBe(payload.street);
+    expect(customer.addresses[0].number).toBe(payload.number);
+    expect(customer.addresses[0].district).toBe(payload.district);
+    expect(customer.addresses[0].city).toBe(payload.city);
+    expect(customer.addresses[0].state).toBe(payload.state);
+    expect(customer.addresses[0].country).toBe(payload.country);
+    expect(customer.addresses[0].zipCode).toBe(payload.zipCode);
+  });
+});
