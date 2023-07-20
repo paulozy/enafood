9c8e77a123c9c6b98ba14115751d50d4f69c08e8
Paulo Abreu <paulo.santana@luby.software>
2023-07-20 11:45:47 -0300

    feat: create remote address from customer usecase

---
+++ src/core/application/usecases/remove-address-from-customer.usecase.ts
@@ -0,0 +1,23 @@
+import { CustomerRepository } from '@domain/repositories/customer-repository.interface';
+import { CustomerNotFoundError } from './@errors/customer-not-found-error';
+
+type RemoveAddressFromCustomerInput = {
+  customerId: string;
+  addressId: string;
+};
+
+export class RemoveAddressFromCustomerUseCase {
+  constructor(private readonly customerRepository: CustomerRepository) {}
+
+  async execute({ customerId, addressId }: RemoveAddressFromCustomerInput) {
+    const customer = await this.customerRepository.findById(customerId);
+
+    if (!customer) {
+      throw new CustomerNotFoundError(customerId);
+    }
+
+    customer.removeAddress(addressId);
+
+    await this.customerRepository.save(customer);
+  }
+}
