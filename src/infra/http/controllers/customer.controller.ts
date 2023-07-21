ed89151258c088ee71ef1df60f500cf6aa3a9907
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 13:01:17 -0300

    feat: create add payment method on customer controller

--- src/infra/http/controllers/customer.controller.ts
+++ src/infra/http/controllers/customer.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { AddPaymentMethodRules } from '../dtos/add-payment-method.dto';
 import { RegisterCustomerRules } from '../dtos/register-customer.dto';
 import { BadRequest } from '../http-responses';
 import { CustomerService } from '../services/customer.service';
+import { AddPaymentMethodValidatorFactory } from '../validators/add-payment-method.validator';
 import { RegisterCustomerValidatorFactory } from '../validators/register-customer.validator';
 
 @Controller('customers')
@@ -20,6 +22,24 @@ export class CustomerController {
     return this.customerService.register({
       ...body,
       plainTextPassword: body.password,
+    });
+  }
+
+  @Post(':id/payment_methods')
+  async addPaymentMethod(
+    @Param('id') customerId: string,
+    @Body() body: AddPaymentMethodRules,
+  ) {
+    const validator = AddPaymentMethodValidatorFactory.create();
+    const isValid = validator.validate(body);
+
+    if (!isValid) {
+      throw new BadRequest(validator.errors);
+    }
+
+    return this.customerService.addPaymentMethod({
+      customerId,
+      ...body,
     });
   }
 }
