9ed306915d470aafd2ffce169f5f673b7257ced9
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 11:18:21 -0300

    feat: create http responses

---
+++ src/infra/http/http-responses.ts
@@ -0,0 +1,19 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class Conflict extends HttpException {
+  constructor(message: string) {
+    super(message, HttpStatus.CONFLICT);
+  }
+}
+
+export class BadRequest extends HttpException {
+  constructor(message: any) {
+    super(message, HttpStatus.BAD_REQUEST);
+  }
+}
+
+export class InternalServerError extends HttpException {
+  constructor() {
+    super('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
