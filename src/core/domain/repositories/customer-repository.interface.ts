6d0d406bd0d6c895727a0c2da65d352c9cbc9df1
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 12:38:55 -0300

    feat: add create abstract method to customer repository interface

--- src/core/domain/repositories/customer-repository.interface.ts
+++ src/core/domain/repositories/customer-repository.interface.ts
@@ -2,6 +2,7 @@ import { Customer } from '@domain/entities/customer.entity';
 
 export abstract class CustomerRepository {
   abstract exists(email: string): Promise<boolean>;
+  abstract create(customer: Customer): Promise<void>;
   abstract save(customer: Customer): Promise<void>;
   abstract findById(id: string): Promise<Customer | undefined>;
   abstract findByEmail(email: string): Promise<Customer | undefined>;
