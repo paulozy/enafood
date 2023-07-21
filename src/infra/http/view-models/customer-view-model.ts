05d4bba5f818a1a5fe5886f2b84153d82837e7eb
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 12:59:14 -0300

    feat: improe customer view-model

--- src/infra/http/view-models/customer-view-model.ts
+++ src/infra/http/view-models/customer-view-model.ts
@@ -2,10 +2,43 @@ import { Customer } from '@domain/entities/customer.entity';
 
 export class CustomerViewModel {
   static toHttp(customer: Customer) {
+    const paymentMethods = customer.paymentMethods.map((paymentMethod) => ({
+      id: paymentMethod.id,
+      method: paymentMethod.paymentMethod,
+      cardNumber: paymentMethod.cardNumber,
+      createdAt: paymentMethod.createdAt,
+      updatedAt: paymentMethod.updatedAt,
+    }));
+
+    const cart = {
+      products: customer.cart.products.map((product) => ({
+        id: product.id,
+        quantity: product.quantity,
+      })),
+      total: customer.cart.total,
+    };
+
+    const addresses = customer.addresses.map((address) => ({
+      id: address.id,
+      street: address.street,
+      number: address.number,
+      complement: address.complement,
+      district: address.district,
+      city: address.city,
+      state: address.state,
+      country: address.country,
+      zipCode: address.zipCode,
+      createdAt: address.createdAt,
+      updatedAt: address.updatedAt,
+    }));
+
     return {
       id: customer.id,
       name: customer.name,
       email: customer.email,
+      cart,
+      paymentMethods,
+      addresses,
       createdAt: customer.createdAt,
       updatedAt: customer.updatedAt,
     };
