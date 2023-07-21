728e9de02081cb02855b58b2499963541ebdd706
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 11:53:14 -0300

    feat: add address usecases and payment usecases to customer usecases factory

--- src/core/application/factories/customer-usecases.factory.ts
+++ src/core/application/factories/customer-usecases.factory.ts
@@ -1,9 +1,15 @@
 import { HasherGateway } from '@domain/gateways/hasher.gateway';
 import { CustomerRepository } from '@domain/repositories/customer-repository.interface';
+import { AddAddressToCustomerUseCase } from '../usecases/add-address-to-customer.usecase';
+import { AddPaymentMethodToCustomerUseCase } from '../usecases/add-payment-method-to-customer.usecase';
 import { RegisterCustomerUseCase } from '../usecases/register-customer.usecase';
+import { RemoveAddressFromCustomerUseCase } from '../usecases/remove-address-from-customer.usecase';
 
 export interface CustomerUseCasesInterface {
   register: RegisterCustomerUseCase;
+  addAddress: AddAddressToCustomerUseCase;
+  removeAddress: RemoveAddressFromCustomerUseCase;
+  addPaymentMethod: AddPaymentMethodToCustomerUseCase;
 }
 
 export const CustomerUseCases = {
@@ -13,6 +19,9 @@ export const CustomerUseCases = {
     hasher: HasherGateway,
   ) => ({
     register: new RegisterCustomerUseCase(customerRepository, hasher),
+    addAddress: new AddAddressToCustomerUseCase(customerRepository),
+    removeAddress: new RemoveAddressFromCustomerUseCase(customerRepository),
+    addPaymentMethod: new AddPaymentMethodToCustomerUseCase(customerRepository),
   }),
   inject: [CustomerRepository, HasherGateway],
 };
