4542a03540dd6f093112892dc900c33dc16c2465
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 11:53:42 -0300

    feat: create cart usecases factory

---
+++ src/core/application/factories/cart-usecases.factory.ts
@@ -0,0 +1,31 @@
+import { CustomerRepository } from '@domain/repositories/customer-repository.interface';
+import { ProductRepository } from '@domain/repositories/product-repository.interface';
+import { AddProductToCartUseCase } from '../usecases/add-product-to-cart.usecase';
+import { DecreaseProductQuantityFromCartUseCase } from '../usecases/decrease-product-quantity-from-cart.usecase';
+import { RemoveProductFromCartUseCase } from '../usecases/remove-product-from-cart.usecase';
+import { ShowCartUseCase } from '../usecases/show-cart.usecase';
+
+export interface CartUseCases {
+  show: ShowCartUseCase;
+  addProduct: AddProductToCartUseCase;
+  removeProduct: RemoveProductFromCartUseCase;
+  decreaseProductQuantity: DecreaseProductQuantityFromCartUseCase;
+}
+
+export const CartUseCases = {
+  provide: 'CartUseCases',
+  useFactory: (
+    customerRepository: CustomerRepository,
+    productRepository: ProductRepository,
+  ) => ({
+    show: new ShowCartUseCase(customerRepository),
+    addProduct: new AddProductToCartUseCase(
+      productRepository,
+      customerRepository,
+    ),
+    removeProduct: new RemoveProductFromCartUseCase(customerRepository),
+    decreaseProductQuantity: new DecreaseProductQuantityFromCartUseCase(
+      customerRepository,
+    ),
+  }),
+};
