8c642f286ab850e207296836817dc5ed8259a6d0
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 23:04:46 -0300

    feat: create add product method on cart controller

--- src/infra/http/controllers/cart.controller.ts
+++ src/infra/http/controllers/cart.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AddProductRules } from '../dtos/add-product.dto';
+import { BadRequest } from '../http-responses';
 import { CartService } from '../services/cart.service';
+import { AddProductValidatorFactory } from '../validators/add-product.validator';
 
 @Controller('cart')
 export class CartController {
@@ -8,5 +11,20 @@ export class CartController {
   @Get(':id')
   async show(@Param('id') customerId: string) {
     return this.cartService.show(customerId);
+  }
+
+  @Post(':id/products')
+  async addProduct(
+    @Param('id') customerId: string,
+    @Body() data: AddProductRules,
+  ) {
+    const validator = AddProductValidatorFactory.create();
+    const isValid = validator.validate(data);
+
+    if (!isValid) {
+      throw new BadRequest(validator.errors);
+    }
+
+    return this.cartService.addProduct({ customerId, item: data });
   }
 }
