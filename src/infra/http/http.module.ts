251896949ed1d7ddf689358c59f60ed244ca5472
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 22:10:44 -0300

    feat: add cart controller to http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,10 +1,13 @@
+import { CartUseCases } from '@core/application/factories/cart-usecases.factory';
 import { CustomerUseCases } from '@core/application/factories/customer-usecases.factory';
 import { Module } from '@nestjs/common';
+import { CartController } from './controllers/cart.controller';
 import { CustomerController } from './controllers/customer.controller';
+import { CartService } from './services/cart.service';
 import { CustomerService } from './services/customer.service';
 
 @Module({
-  controllers: [CustomerController],
-  providers: [CustomerService, CustomerUseCases],
+  controllers: [CustomerController, CartController],
+  providers: [CustomerService, CustomerUseCases, CartService, CartUseCases],
 })
 export class HttpModule {}
