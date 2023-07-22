8b0af6793f3389a5a8cb8d5fbc28cfb74a45a552
Paulo Abreu <paulo.santana@luby.software>
2023-07-22 01:46:49 -0300

    feat: add security config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './infra/database/database.module';
@@ -7,8 +9,23 @@ import { GatewayModule } from './infra/gateways/gateway.module';
 import { HttpModule } from './infra/http/http.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, GatewayModule, HttpModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+    DatabaseModule,
+    GatewayModule,
+    HttpModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   setupSwagger(app);
+  app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
