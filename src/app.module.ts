c64035ba35c442fb67b4c4d1c14752a84594b719
Paulo Abreu <paulo.santana@luby.software>
2023-07-22 02:08:46 -0300

    refactor: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { DatabaseModule } from './infra/database/database.module';
 import { GatewayModule } from './infra/gateways/gateway.module';
 import { HealthModule } from './infra/health/health.module';
@@ -21,9 +20,7 @@ import { HttpModule } from './infra/http/http.module';
     HttpModule,
     HealthModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
