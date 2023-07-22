83f2108dd6e941386f324f04962e66f322df0f3b
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 22:38:13 -0300

    feat: create prisma product seed

---
+++ prisma/seeds/product-seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+import { products } from '@shared/utils/products';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for (const product of products) {
+    await prisma.product.create({
+      data: product,
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (error) => {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
