5c532259879cd3c61cb7072f934cf29a96c5a996
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 23:41:33 -0300

    feat: create checkout controller

---
+++ src/infra/http/controllers/checkout.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { CheckoutRules } from '../dtos/checkout.dto';
+import { BadRequest } from '../http-responses';
+import { CheckoutService } from '../services/checkout.service';
+import { CheckoutValidatorFactory } from '../validators/checkout.validator';
+
+@Controller('checkout')
+export class CheckoutController {
+  constructor(private readonly checkoutService: CheckoutService) {}
+
+  @Post(':id')
+  async checkout(@Param('id') customerId: string, @Body() data: CheckoutRules) {
+    const validator = CheckoutValidatorFactory.create();
+    const isValid = validator.validate(data);
+
+    if (!isValid) {
+      throw new BadRequest(validator.errors);
+    }
+
+    return this.checkoutService.checkout({
+      customerId,
+      CVC: data.CVC,
+      paymentMethodId: data.paymentMethodId,
+    });
+  }
+}
