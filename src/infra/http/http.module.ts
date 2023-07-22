289367e28ac23201197e5c484744d66ae5513b59
Paulo Abreu <paulo.santana@luby.software>
2023-07-22 01:01:06 -0300

    feat: add product controller on http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,15 +2,18 @@ import { CartUseCases } from '@core/application/factories/cart-usecases.factory'
 import { CheckoutUseCases } from '@core/application/factories/checkout-usecases.factory';
 import { CustomerUseCases } from '@core/application/factories/customer-usecases.factory';
 import { OrderUseCases } from '@core/application/factories/order-usecases.factory';
+import { ProductUseCases } from '@core/application/factories/product-usecases.factory';
 import { Module } from '@nestjs/common';
 import { CartController } from './controllers/cart.controller';
 import { CheckoutController } from './controllers/checkout.controller';
 import { CustomerController } from './controllers/customer.controller';
 import { OrderController } from './controllers/order.controller';
+import { ProductController } from './controllers/product.controller';
 import { CartService } from './services/cart.service';
 import { CheckoutService } from './services/checkout.service';
 import { CustomerService } from './services/customer.service';
 import { OrderService } from './services/order.service';
+import { ProductService } from './services/product.service';
 
 @Module({
   controllers: [
@@ -18,6 +21,7 @@ import { OrderService } from './services/order.service';
     CartController,
     CheckoutController,
     OrderController,
+    ProductController,
   ],
   providers: [
     CustomerService,
@@ -28,6 +32,8 @@ import { OrderService } from './services/order.service';
     CheckoutUseCases,
     OrderService,
     OrderUseCases,
+    ProductService,
+    ProductUseCases,
   ],
 })
 export class HttpModule {}
