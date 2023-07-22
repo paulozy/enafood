4fefc647678977913a1a0a1fc931364e6e0bda2a
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 23:39:41 -0300

    feat: create fake payment gateway

---
+++ src/infra/gateways/payment/payment.service.ts
@@ -0,0 +1,52 @@
+import {
+  PaymentGateway,
+  PaymentResponse,
+} from '@domain/gateways/payment.gateway';
+import { Injectable } from '@nestjs/common';
+import { randomUUID as uuid } from 'node:crypto';
+
+enum PaymentStatus {
+  APPROVED = 'APPROVED',
+  DECLINED = 'DECLINED',
+}
+
+const possibleReasons = [
+  'Insufficient funds',
+  'Invalid card number',
+  'Invalid CVC',
+];
+
+@Injectable()
+export class FakePaymentGateway implements PaymentGateway {
+  async pay(): Promise<PaymentResponse> {
+    const randomNum = Math.floor(Math.random() * 10);
+
+    if (randomNum % 2 === 0) {
+      return this.approvePayment();
+    }
+
+    return this.declinePayment();
+  }
+
+  private async approvePayment(): Promise<PaymentResponse> {
+    const payment = {
+      id: uuid(),
+      status: PaymentStatus.APPROVED,
+    };
+
+    return payment;
+  }
+
+  private async declinePayment(): Promise<PaymentResponse> {
+    const reason =
+      possibleReasons[Math.floor(Math.random() * possibleReasons.length)];
+
+    const payment = {
+      id: uuid(),
+      status: PaymentStatus.DECLINED,
+      reason,
+    };
+
+    return payment;
+  }
+}
