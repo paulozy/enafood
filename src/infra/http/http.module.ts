e89007d978133649728703da213ef059cae7a458
Paulo Abreu <paulo.santana@luby.software>
2023-07-21 23:41:47 -0300

    feat: add checkout controller to http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,13 +1,23 @@
 import { CartUseCases } from '@core/application/factories/cart-usecases.factory';
+import { CheckoutUseCases } from '@core/application/factories/checkout-usecases.factory';
 import { CustomerUseCases } from '@core/application/factories/customer-usecases.factory';
 import { Module } from '@nestjs/common';
 import { CartController } from './controllers/cart.controller';
+import { CheckoutController } from './controllers/checkout.controller';
 import { CustomerController } from './controllers/customer.controller';
 import { CartService } from './services/cart.service';
+import { CheckoutService } from './services/checkout.service';
 import { CustomerService } from './services/customer.service';
 
 @Module({
-  controllers: [CustomerController, CartController],
-  providers: [CustomerService, CustomerUseCases, CartService, CartUseCases],
+  controllers: [CustomerController, CartController, CheckoutController],
+  providers: [
+    CustomerService,
+    CustomerUseCases,
+    CartService,
+    CartUseCases,
+    CheckoutService,
+    CheckoutUseCases,
+  ],
 })
 export class HttpModule {}
