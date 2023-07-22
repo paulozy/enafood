7ef5fb369106d49e9ebc372b4753ef52b5eb2948
Paulo Abreu <paulo.santana@luby.software>
2023-07-22 00:10:19 -0300

    feat: add order controller to http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,16 +1,24 @@
 import { CartUseCases } from '@core/application/factories/cart-usecases.factory';
 import { CheckoutUseCases } from '@core/application/factories/checkout-usecases.factory';
 import { CustomerUseCases } from '@core/application/factories/customer-usecases.factory';
+import { OrderUseCases } from '@core/application/factories/order-usecases.factory';
 import { Module } from '@nestjs/common';
 import { CartController } from './controllers/cart.controller';
 import { CheckoutController } from './controllers/checkout.controller';
 import { CustomerController } from './controllers/customer.controller';
+import { OrderController } from './controllers/order.controller';
 import { CartService } from './services/cart.service';
 import { CheckoutService } from './services/checkout.service';
 import { CustomerService } from './services/customer.service';
+import { OrderService } from './services/order.service';
 
 @Module({
-  controllers: [CustomerController, CartController, CheckoutController],
+  controllers: [
+    CustomerController,
+    CartController,
+    CheckoutController,
+    OrderController,
+  ],
   providers: [
     CustomerService,
     CustomerUseCases,
@@ -18,6 +26,8 @@ import { CustomerService } from './services/customer.service';
     CartUseCases,
     CheckoutService,
     CheckoutUseCases,
+    OrderService,
+    OrderUseCases,
   ],
 })
 export class HttpModule {}
